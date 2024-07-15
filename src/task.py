55019cb25c93bfb4989b7840d31d883bfc3a52ee
charudatta <152109007c@gmail.com>
2024-07-15 23:36:59 +0530

    restructure added cli feature

--- src/task.py
+++ src/task.py
@@ -1,48 +1,51 @@
 from pathlib import Path
 import json
+import fire
+import pandas as pd
 
 
 class task_manager:
 
-    def __init__(self, file="task.json") -> None:
-        self.file = Path(file)
+    def __init__(self) -> None:
         self.task_list = {}
         self.task_id = 0
+        self.add_file(file="todo.json")
+
+    def add_file(self, file="todo.json"):
+        self.file = Path(file)
         if self.file.exists():
-            with open(self.file,"r") as f:
+            with open(self.file, "r") as f:
                 self.task_list = json.load(f)
                 self.task_id = len(self.task_list)
         else:
             with open(self.file, "w") as f:
-                json.dump(self.task_list,f)
+                json.dump(self.task_list, f)
 
     def add_task(self, task, state="todo", tags=None, note=None):
-        #tags 
-        #tags with #
-        #Context with @ 
-        #project with +
-        #priority with !
-        #deadline with *
-        #repeat with ~ 
+        # tags
+        # tags with #
+        # Context with @
+        # project with +
+        # priority with !
+        # deadline with *
+        # repeat with ~
         self.task_list[str(self.task_id)] = {
             "task": task,
             "state": state,
             "tags": tags,
             "note": note,
         }
-        self.task_id =  self.task_id + 1
-        self._save_tasks()
+        self.task_id = self.task_id + 1
+        self.save_tasks()
 
     def view_task(self):
-        print("| ID | task | state | eot | span | tags | note |")
-        print("----------------------------------------------------------")
-        for key, values in self.task_list.items():
-            print(f"| {key} | {values["task"]} | {values["state"]} | {values["tags"]} | {values["note"]} | ")
+        df = pd.DataFrame.from_dict(self.task_list).transpose()
+        print(df)
 
     def del_task(self, task_id):
         try:
             del self.task_list[str(task_id)]
-            self._save_tasks()
+            self.save_tasks()
         except:
             print("Task does not exist")
 
@@ -50,42 +53,58 @@ class task_manager:
         sorted_dict = {}
         for key in sorted(self.task_list, key=lambda k: self.task_list[k][sort_by]):
             sorted_dict[key] = self.task_list[key]
-        print(sorted_dict)
+        print(pd.DataFrame.from_dict(sorted_dict).transpose())
 
     def sort_tags(self, sort_tags="@me"):
-        sorted_task_list = []
+        keys = []
         for key, values in self.task_list.items():
             if values["tags"] == None:
                 continue
-            elif sort_tags in  values["tags"]:
-                sorted_task_list.append(key) 
-        print([self.task_list[x] for x in sorted_task_list])
+            elif sort_tags in values["tags"]:
+                keys.append(key)
+        # print([self.task_list[x] for x in sorted_task_list])
+        print(
+            pd.DataFrame.from_dict(
+                {key: self.task_list[key] for key in keys}
+            ).transpose()
+        )
 
     def edit_task(self, task_id, task_key, task_value):
         try:
-            self.task_list[str(task_id)][task_key]= task_value
-            self._save_tasks()
+            if str(task_id) in self.task_list:
+                if task_key in self.task_list[str(task_id)]:
+                    self.task_list[str(task_id)][task_key] = task_value
+                    self.save_tasks()
+                else:
+                    print("Task key doesn't exist")
+            else:
+                print("Task id doesn't exist")
         except:
-            print("Error either task id is invalid or task key doesn't exist")
+            print("Unknown Error")
 
+    def edit_status(self, task_id, task_value="done"):
+        try:
+            self.task_list[str(task_id)]["status"] = task_value
+            self.save_tasks()
+        except:
+            print("Error task id is invalid")
 
-    def _save_tasks(self):
+    def save_tasks(self):
         with open(self.file, "w") as f:
-                json.dump(self.task_list,f)
-
+            json.dump(self.task_list, f)
 
 
 if __name__ == "__main__":
-    t1 = task_manager("task.json")
-    t1.add_task("get milk")
-    t1.add_task("get grocery")
-    t1.view_task()
-    t1.edit_task(1,"tags",["@me","+patent","*tomorrow"])
-    t1.edit_task(10,"tags",["@suma","+patent","*tomorrow","@me"])
-    t1.edit_task(7,"tags",["@me","+patent","*tomorrow"])
-    t1.edit_task(13,"tags",["@me","+patent","*tomorrow"])
-    t1.edit_task(17,"tags",["@u","+patent","*tomorrow"])
-    t1.edit_task(1,"tags",["@to","+patent","*tomorrow"])
-    t1.view_task()
-    t1.sort_task()
-    t1.sort_tags()
+    fire.Fire(
+        {
+            "a": task_manager().add_task,
+            "b": task_manager().sort_task,
+            "c": task_manager().edit_status,
+            "d": task_manager().del_task,
+            "e": task_manager().edit_task,
+            "f": task_manager().add_file,
+            "r": task_manager().view_task,
+            "s": task_manager().save_tasks,
+            "t": task_manager().sort_tags,
+        }
+    )
