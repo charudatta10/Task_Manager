9e8c5de6f1d51844b11c8dab912e937798dd1d7f
charudatta <152109007c@gmail.com>
2024-07-16 14:42:02 +0530

    added main file

---
+++ __main__.py
@@ -0,0 +1,42 @@
+from flask import Flask
+import fire
+from src.task import task_manager
+
+
+def api():  
+    app = Flask(__name__)
+
+    @app.route("/add")
+    def add():
+        pass
+
+    app.run(debug=True)
+
+
+def cli():
+    fire.Fire(
+        {
+            "a": task_manager().add_task,
+            "b": task_manager().sort_task,
+            "c": task_manager().edit_status,
+            "d": task_manager().del_task,
+            "e": task_manager().edit_task,
+            "f": task_manager().add_file,
+            "r": task_manager().view_task,
+            "s": task_manager().save_tasks,
+            "t": task_manager().sort_tags,
+        }
+    )
+
+
+
+
+def main():
+    #cli()
+    api()
+
+    
+
+if "__name__" == "__main__":
+    print("project Todo List Task manager")
+    main()
